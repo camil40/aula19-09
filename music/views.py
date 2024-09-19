59fec2b5bcad31ea77b71a20236246b1961b1c11
camil40 <camilamarinsmatos@outlook.com>
2024-09-19 19:41:41 +0000

    Aula bem legal sobre musicas de sofrencia e djangorestfull

---
+++ music/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Music
+from .serializers import MusicSerializer
+
+class MusicViewSet(viewsets.ModelViewSet):
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer
+    
